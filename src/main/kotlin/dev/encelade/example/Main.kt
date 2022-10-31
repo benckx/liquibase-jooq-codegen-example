ce691ac22191510c0eb26d534f75be9a0339d831
benckx <benoit.vleminckx@restore.eu>
2022-10-31 13:07:07 +0100

    upgrade libs

--- src/main/kotlin/dev/encelade/example/Main.kt
+++ src/main/kotlin/dev/encelade/example/Main.kt
@@ -5,13 +5,17 @@ import dev.encelade.example.dao.codegen.tables.pojos.Person
 
 fun main() {
     val dslContext = DaoService.getDslContext("example.db")
-    val cfg = dslContext.configuration()
-    val personDao = PersonDao(cfg)
 
-    val person = Person()
-    person.firstName = "Charles"
-    person.lastName = "Baudelaire"
-    personDao.insert(person)
+    dslContext.transaction { cfg ->
+        val personDao = PersonDao(cfg)
+        val person = Person()
+        person.firstName = "Charles"
+        person.lastName = "Baudelaire"
+        personDao.insert(person)
+    }
 
-    println("entries: ${personDao.count()}")
+    dslContext.transaction { cfg ->
+        val personDao = PersonDao(cfg)
+        println("entries: ${personDao.count()}")
+    }
 }
